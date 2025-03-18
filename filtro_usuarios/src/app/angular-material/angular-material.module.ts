1cb41a2f6d0719ee1e0f66d3c22ff58ec1709048
santivgo <requiemsantiago@gmail.com>
2025-03-18 10:47:32 -0300

    wip: tipografia

--- filtro_usuarios/src/app/angular-material/angular-material.module.ts
+++ filtro_usuarios/src/app/angular-material/angular-material.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 @NgModule({
   declarations: [],
   imports: [
-    MatTableModule, MatButtonModule, MatCardModule
+    MatTableModule, MatButtonModule, MatCardModule, MatDividerModule
   ],
   exports: [
-    MatButtonModule, MatTableModule, MatCardModule
+    MatButtonModule, MatTableModule, MatCardModule, MatDividerModule
   ]
 })
 export class AngularMaterialModule { }
