a6a38c595a585030a74c1daaa7318cbf32d51f4a
santivgo <requiemsantiago@gmail.com>
2025-03-19 17:39:16 -0300

    criando a barra de menu

--- filtro_usuarios/src/app/angular-material/angular-material.module.ts
+++ filtro_usuarios/src/app/angular-material/angular-material.module.ts
@@ -3,13 +3,21 @@ import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+
+
+
 @NgModule({
   declarations: [],
   imports: [
-    MatTableModule, MatButtonModule, MatCardModule, MatDividerModule
+    MatTableModule, MatNativeDateModule, MatButtonModule, MatCardModule, MatDividerModule, MatDatepickerModule, MatFormFieldModule, MatSelectModule, MatInputModule
   ],
   exports: [
-    MatButtonModule, MatTableModule, MatCardModule, MatDividerModule
+    MatButtonModule, MatNativeDateModule, MatTableModule, MatCardModule, MatDividerModule, MatDatepickerModule, MatFormFieldModule, MatSelectModule, MatInputModule
   ]
 })
 export class AngularMaterialModule { }
--- filtro_usuarios/src/app/components/components.module.ts
+++ filtro_usuarios/src/app/components/components.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
 import { MenuComponent } from './menu/menu.component';
-import { FiltrarButtonComponent } from './filtrar-button/filtrar-button.component';
 import { DashboardUserComponent } from './dashboard-user/dashboard-user.component';
 import { UserTableComponent } from './user-table/user-table.component';
 
 
 
 @NgModule({
-  declarations: [MenuComponent, FiltrarButtonComponent, DashboardUserComponent, UserTableComponent],
+  declarations: [MenuComponent, DashboardUserComponent, UserTableComponent],
   imports: [
     AngularMaterialModule,
   ],
   exports: [
     AngularMaterialModule,
-    MenuComponent, FiltrarButtonComponent, DashboardUserComponent, UserTableComponent
+    MenuComponent, DashboardUserComponent, UserTableComponent
   ]
 })
 export class ComponentsModule { }
