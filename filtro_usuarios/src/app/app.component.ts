086dc7b38509e635255f6bdd955db64cf1904f4a
santivgo <requiemsantiago@gmail.com>
2025-03-25 10:36:02 -0300

    filtro de lista por nome

--- filtro_usuarios/src/app/app.component.ts
+++ filtro_usuarios/src/app/app.component.ts
@@ -12,21 +12,53 @@ import { IFilterOptions } from './interfaces/filter.interface';
 export class AppComponent implements OnInit {
 
   listaUsuarios: IUser[] = [];
+  listaUsuariosFiltradas: IUser[] = []
   usuarioSelecionado: IUser = {} as IUser;
   showUserDetails: boolean = false;
 
   ngOnInit(): void { // Assim que o componente carrega, ele executa a função que eu inserir aqui
     setTimeout(() => {
       this.listaUsuarios = Usuarios
-    }, 3000)
+      this.listaUsuariosFiltradas = this.listaUsuarios;
+    }, 1)
+  }
+
+  filterListByName(name: string | undefined, listaFiltrada: IUser[]): IUser[] {
+    //filter sempre retorna um array
+    const NOME_N_TIPADO = name === undefined
+
+    if (NOME_N_TIPADO) {
+      return listaFiltrada
+    }
+
+    return listaFiltrada.filter((p) => p.nome && name && p.nome.toLowerCase().includes(name.toLowerCase()));
+
+  }
+  filtrarLista(filtro: IFilterOptions, listaFiltrada: IUser[]): IUser[] { // Função Pura, não modifica variaveis externas e não precisa de VARIÁVEIS EXTERNAS pra funcionar.
+
+    let filteredList: IUser[] = listaFiltrada;
+    filteredList = this.filterListByName(filtro.name, listaFiltrada)
+    return filteredList
+
   }
 
   getFiltros(filterObject: IFilterOptions) {
-    console.log(filterObject)
+    this.listaUsuariosFiltradas = this.listaUsuarios
+    const INVALID_FILTER = !filterObject.name && !filterObject.startDate && !filterObject.endDate && !filterObject.status
+
+    if (INVALID_FILTER) {
+      this.listaUsuariosFiltradas = this.listaUsuarios;
+      return
+    }
+    this.listaUsuariosFiltradas = this.filtrarLista(filterObject, this.listaUsuariosFiltradas)
 
   }
+
+
   userClicked(usuario: IUser) {
     this.usuarioSelecionado = usuario;
     this.showUserDetails = true;
   }
+
+
 }
