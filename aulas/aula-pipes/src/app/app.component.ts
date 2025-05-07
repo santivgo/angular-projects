0b8e9f765ada4c5dde29b3070e4bad8ac91c190d
santivgo <requiemsantiago@gmail.com>
2025-05-07 11:53:05 -0300

    locale

--- aulas/aula-pipes/src/app/app.component.ts
+++ aulas/aula-pipes/src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { CommonModule, DatePipeConfig } from '@angular/common';
+import { Component, LOCALE_ID, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StatusPipe } from './pipes/status.pipe';
 import { userStatusEnum } from './enums/userStatusEnum';
 import { TruncatePipe } from './pipes/truncate.pipe';
+import { DATE_PIPE_DEFAULT_OPTIONS } from '@angular/common';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
+registerLocaleData(localePt)
+
+
+const datePipeConfig: DatePipeConfig = {dateFormat: 'dd/MM/YYYY', timezone: '-0003'}
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, CommonModule, StatusPipe, TruncatePipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
+  providers: [{provide:DATE_PIPE_DEFAULT_OPTIONS, useValue: datePipeConfig}, {provide: LOCALE_ID, useValue: 'pt-BR'}]
 })
 export class AppComponent implements OnInit{
   pessoa = {
