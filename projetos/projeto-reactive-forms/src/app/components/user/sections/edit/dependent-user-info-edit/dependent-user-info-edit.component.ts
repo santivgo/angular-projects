7fe4afdd80c21bf6210265780d82ec527dd1f119
santivgo <requiemsantiago@gmail.com>
2025-09-25 09:55:19 -0300

    output de depedente

--- projetos/projeto-reactive-forms/src/app/components/user/sections/edit/dependent-user-info-edit/dependent-user-info-edit.component.ts
+++ projetos/projeto-reactive-forms/src/app/components/user/sections/edit/dependent-user-info-edit/dependent-user-info-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DependentList } from '../../../../../types/dependent-list.type';
 import { Form, FormArray, FormGroup } from '@angular/forms';
 import { UserFormController } from '../../../forms/user-form-controller';
@@ -9,9 +9,25 @@ import { UserFormController } from '../../../forms/user-form-controller';
   templateUrl: './dependent-user-info-edit.component.html',
   styleUrl: './dependent-user-info-edit.component.sass'
 })
-export class DependentUserInfoEditComponent extends UserFormController {
-  @Input({ required: true, alias: 'userInfoForm' }) userInfoF!: FormGroup
+export class DependentUserInfoEditComponent {
+  @Input({ required: true, alias: 'userInfoForm' }) userInfoForm!: FormGroup
 
+  get dependentList(): FormArray {
+    return this.userInfoForm.get('dependentList') as FormArray
+  }
+  @Output('onRemoveDependent') onRemoveDependentEmitt: EventEmitter<number> = new EventEmitter<number>
+  @Output('onCreateDependent') onCreateDependentEmitt: EventEmitter<void> = new EventEmitter<void>
+
+
+
+  removeDependent(i: number): void {
+    this.onRemoveDependentEmitt.emit(i)
+  }
+
+
+  createEmptyDependent(): void {
+    this.onCreateDependentEmitt.emit()
+  }
 
 
 }
